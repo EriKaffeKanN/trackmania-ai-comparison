130026afe1b90fcd5e6f0403351b9b7796c68826
EriKaffeKanN <erik.andersson0901@gmail.com>
2021-09-22 21:11:27 +0200

    Untested loop

--- client/image-processing-module/screenshot-handler.py
+++ client/image-processing-module/screenshot-handler.py
@@ -6,42 +6,46 @@ from pynput import keyboard
 from PIL import ImageEnhance
 import time
 
-def getProcessedScreenshot():
-    im = pyautogui.screenshot()
-    colorEnhancer = ImageEnhance.Color(im)
-    monochrome = colorEnhancer.enhance(0) # Black and white
-    contrastEnhancer = ImageEnhance.Contrast(monochrome)
-    processedIm = contrastEnhancer.enhance(10) # Arbitrary factor to increase contrast to a maximum
-    return processedIm
+class ImageProcessingModule:
 
-record = False
+    @staticmethod
+    def record():
+        t = time.time()
+        getProcessedScreenshot()
+        dt = time.time() - t
+        print("Elapsed Screenshot Time: {0}".format(dt))
+
+    @staticmethod
+    def getProcessedScreenshot():
+        im = pyautogui.screenshot()
+        colorEnhancer = ImageEnhance.Color(im)
+        monochrome = colorEnhancer.enhance(0) # Black and white
+        contrastEnhancer = ImageEnhance.Contrast(monochrome)
+        processedIm = contrastEnhancer.enhance(10) # Arbitrary factor to increase contrast to a maximum
+        return processedIm
 
 def onKeyPress(key):
-    global record
-
-    try:
-        print('alphanumeric key {0} pressed'.format(key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(key))
-    
-        if key == keyboard.Key.home:
-            record = True
-        if key == keyboard.Key.page_up:
-            record = False
-        if record:
-            t = time.time()
-            #getProcessedScreenshot()
-            dt = time.time() - t
-            print(f"Elapsed Screenshot Time: {dt}")
+    if key == keyboard.Key.home:
+        recording = True
+        with keyboard.Listener(
+            on_press=awaitExit
+            on_release=onKeyRelease
+        ) as exitListener:
+            exitListener.join()
+        while recording:
+            def awaitExit(key2):
+                if key2 == keyboard.Key.page_up:
+                    recording = False
+            ImageProcessingModule.record()
 
 def onKeyRelease(key):
-    global record
-
     if key == keyboard.Key.end:
         # Stop listener
         return False
+            
+if __name__ == "main":
+    with keyboard.Listener(
+            on_press=onKeyPress,
+            on_release=onKeyRelease) as listener:
+        listener.join()
 
-with keyboard.Listener(
-        on_press=onKeyPress,
-        on_release=onKeyRelease) as listener:
-    listener.join()
