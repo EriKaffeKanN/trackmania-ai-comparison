02b1e4b2b3222e64fd8785b06cd52d1b3031c9ca
EriKaffeKanN <errik.jag@gmail.com>
2021-09-28 10:28:53 +0200

    Update screenshot-handler.py

--- client/image-processing-module/screenshot-handler.py
+++ client/image-processing-module/screenshot-handler.py
@@ -1,22 +1,28 @@
 # Image Processing
 
 # Imports
+from PIL.Image import Image
 import pyautogui
-from pynput import keyboard
+import keyboard
 from PIL import ImageEnhance
 import time
+import math
+
+class Ray:
+    def __init__(self) -> None:
+        pass
 
 class ImageProcessingModule:
 
     @staticmethod
-    def record():
+    def record() -> None:
         t = time.time()
         ImageProcessingModule.getProcessedScreenshot()
         dt = time.time() - t
         print("Elapsed Screenshot Time: {0}".format(dt))
 
     @staticmethod
-    def getProcessedScreenshot():
+    def getProcessedScreenshot() -> Image:
         im = pyautogui.screenshot()
         colorEnhancer = ImageEnhance.Color(im)
         monochrome = colorEnhancer.enhance(0) # Black and white
@@ -24,33 +30,22 @@ class ImageProcessingModule:
         processedIm = contrastEnhancer.enhance(10) # Arbitrary factor to increase contrast to a maximum
         return processedIm
 
-def onKeyPress(key):
-    print(key)
-    if key == keyboard.Key.home:
-        recording = True
-        firstIteration = True
-        def awaitExit(key2):
-            global recording
-            if key2 == keyboard.Key.page_up:
-                recording = False # FIX: Function is called but recording is unchanged
-        while recording:
-            if firstIteration:
-                with keyboard.Listener(
-                    on_press=awaitExit,
-                    on_release=onKeyRelease
-                ) as exitListener:
-                    exitListener.join()
-                    firstIteration = False
-            
-            ImageProcessingModule.record()
+    @staticmethod
+    def getLineLengths(img, angleMin, angleMax, angleStep) -> list[int]:
+        pixelMap = img.load()
+        baseCoordinates = [img.size[0]//2, img.size[1]-1]
+        rayQuantity = (angleMax-angleMin)//angleStep
+        for i in range(rayQuantity):
+            theta = angleMin + angleStep*i
+            stepX = math.cos(theta)
+            stepY = math.sin(theta)
+        return None
 
-def onKeyRelease(key):
-    if key == keyboard.Key.end:
-        # Stop listener
-        return False
-            
-with keyboard.Listener(
-    on_press=onKeyPress,
-    on_release=onKeyRelease) as listener:
-    listener.join()
 
+while True:
+    if keyboard.is_pressed("home"):
+        while not keyboard.is_pressed("end"):
+            ImageProcessingModule.getLineLengths(ImageProcessingModule.getProcessedScreenshot(), 0, 0, 0)
+        print("Done recording")
+    if keyboard.is_pressed("esc"):
+        break
