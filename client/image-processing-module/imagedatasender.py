0f3ac4672c386b9c26f561d71cc5cacc3bd988a5
EriKaffeKanN <errik.jag@gmail.com>
2021-10-13 10:35:40 +0200

    Bug fix

--- client/image-processing-module/imagedatasender.py
+++ client/image-processing-module/imagedatasender.py
@@ -16,14 +16,14 @@ print("Server response: ", repr(data))
 while True:
     if keyboard.is_pressed("home"):
         while not keyboard.is_pressed("end"):
-            IMP.getLineLengths(IMP.getProcessedScreenshot(), 0, 0, 0)
+            img = IMP.getProcessedScreenshot()
+            IMP.getLineLengths(img, math.pi/12, math.pi-(math.pi/12), 5*math.pi/(6*11))
         print("Done recording")
     if keyboard.is_pressed("o"):
-        img = IMP.getProcessedScreenshot()
-        lines = IMP.getLineLengths(img, math.pi/12, math.pi-(math.pi/12), 5*math.pi/(6*12))
+        lines = IMP.getLineLengths(img, math.pi/12, math.pi-(math.pi/12), 5*math.pi/(6*11))
         print("Line values:")
         for l in lines:
             print(l)
-        IMP.visualizeLines(img, lines, math.pi/12, math.pi-(math.pi/12),  5*math.pi/(6*12))
+        IMP.visualizeLines(img, lines, math.pi/12, math.pi-(math.pi/12),  5*math.pi/(6*11))
     if keyboard.is_pressed("esc"):
         break
